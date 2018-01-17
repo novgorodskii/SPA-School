f97b4e3ffd069668c6545a2b1cc5f80b547cd742
novgorodskii <novgorodskiji@gmail.com>
2018-01-17 21:54:28 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// tabs
+$(".tab_item").not(":first").hide();
+$(".wrapper .tab").click(function() {
+	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
